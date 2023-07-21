ea276693e694fd5b6610eb591c18ec5a3c7e98ac
Joel Haasnoot <joelhaasnoot@gmail.com>
2023-07-21 16:23:59 +0200

    Refactor and document

--- functions.py
+++ functions.py
@@ -17,7 +17,10 @@ def async_process(msg, name, config=None):
     message = GzipFile('', 'r', 0, BytesIO(contents)).read()
     topic = name + "/" + address.lstrip('/')
 
+    # This is the original message
     messages_out = [(topic, message, False)]
+
+    # See if we also want to send a second copy on another topic
     if config is not None and address in config:
         topic_config = config[address]
         tree = etree.parse(BytesIO(message))
@@ -30,7 +33,8 @@ def async_process(msg, name, config=None):
         messages_out.append((new_topic, message, True))
     return messages_out
 
-
+expiry_properties = Properties(PacketTypes.PUBLISH)
+expiry_properties.MessageExpiryInterval = 60*60*28  # retain messages for 24 + 4 hours
 
 async def recv_and_process(ctx, port, name, config=None):
     sock = ctx.socket(zmq.SUB)
@@ -48,8 +52,6 @@ async def recv_and_process(ctx, port, name, config=None):
         msg = await sock.recv_multipart()  # waits for msg to be ready
         parsed = async_process(msg, name, config)  # We can return multiple messages
         for (topic, payload, retain) in parsed:
-            properties = Properties(PacketTypes.PUBLISH)
-            properties.MessageExpiryInterval = 60*60*28  # retain messages for 24 + 4 hours
-            await mqtt.asyncio_publish(topic, payload, retain=retain, properties=properties if retain else None)
+            await mqtt.asyncio_publish(topic, payload, retain=retain, properties=expiry_properties if retain else None)
 
     mqtt.Disconnect()
